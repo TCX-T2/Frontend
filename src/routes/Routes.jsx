fb709913a5d7b4582067108334e3a0bd078248e1
Raouf <raoufslv09@gmail.com>
2024-01-06 11:16:06 +0100

    forget to add a route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Signin from "../pages/Signin.jsx";
 import Signup from "../pages/Signup.jsx";
 import Home from "../pages/Home.jsx";
-import Profile from "../pages/Profile.jsx";
+import Profile from "../components/Profile.jsx";
 import Patient from "../pages/Patient.jsx";
 
 const Routers = () => {
@@ -14,8 +14,9 @@ const Routers = () => {
       <Route path="/signup" element={<Signup />}></Route>
       <Route path="/dashboard" element={<Home />}></Route>
       <Route path="/historique" element={<Home />}></Route>
+      <Route path="/calendar" element={<Home />}></Route>
       <Route path="/addpatient" element={<Home />}></Route>
-      <Route path="/profile" element={<Profile />}></Route>
+      <Route path="/profile" element={<Home />}></Route>
       <Route path="/patient" element={<Patient />}></Route>
     </Routes>
   );
